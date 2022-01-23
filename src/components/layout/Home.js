b219c71987439afa39dbff2e42cba2eec66d2939
music-tko <thandokhoase26@gmail.com>
2022-01-23 12:23:15 +0200

    final image

--- src/components/layout/Home.js
+++ src/components/layout/Home.js
@@ -1,10 +1,15 @@
 import NavBar from "../navigation/NavBar";
-
+import { Link } from "react-router-dom";
 const Home = () => {
 	return (
 		<div className='mainHome'>
 			<NavBar />
-			<div className='header'></div>
+			<div className='header'>
+				<h1>Check Out Our Designers</h1>
+				<Link to='/stores'>
+					<button>Go To Store</button>
+				</Link>
+			</div>
 		</div>
 	);
 };
